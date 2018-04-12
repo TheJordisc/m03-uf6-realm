16042b23771f2766c722a82adb2963d34f838222
TheJordisc <jsolaceada@gmail.com>
2018-04-12 20:07:28 +0200

    Fixed layout

--- app/src/main/java/com/example/jordi/m03_uf6_realm/view/MainActivity.java
+++ app/src/main/java/com/example/jordi/m03_uf6_realm/view/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.jordi.m03_uf6_realm.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.jordi.m03_uf6_realm.R;
 import com.example.jordi.m03_uf6_realm.model.Persona;
 
-import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Locale;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -22,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         Realm.init(this);
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
         Realm.setDefaultConfiguration(realmConfiguration);
@@ -30,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
         realm.beginTransaction();
 
+        realm.deleteAll();
         Persona persona = new Persona();
+        persona.setDni("45454545B");
         persona.setNom("Jordi");
         persona.setCognom("Solà Ceada");
         persona.setGenere("M");
@@ -40,5 +56,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         realm.commitTransaction();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
