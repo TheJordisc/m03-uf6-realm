f4d56d63809b24c91fca7ea3bee0ed12a546f2fd
dam2a <alejandrokad@gmail.com>
2018-04-17 16:37:01 +0200

    Fixed filter button and added AlertDialog

---
+++ app/src/main/java/com/example/jordi/m03_uf6_realm/view/ActivityNewContact.java
@@ -0,0 +1,31 @@
+package com.example.jordi.m03_uf6_realm.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.jordi.m03_uf6_realm.R;
+
+public class ActivityNewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_contact);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
