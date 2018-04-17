c0097dd6dd31247ab5b7c55f8955526efcb93f0e
dam2a <alejandrokad@gmail.com>
2018-04-17 16:41:35 +0200

    Added newContactActivity

--- app/src/main/java/com/example/jordi/m03_uf6_realm/view/MainActivity.java
+++ app/src/main/java/com/example/jordi/m03_uf6_realm/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jordi.m03_uf6_realm.view;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -93,8 +94,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, ActivityNewContact.class);
+                startActivity(intent);
             }
         });
     }
